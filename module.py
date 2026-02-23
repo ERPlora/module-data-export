f2dcf04b16adca108a76aeda5a3f4e5a27831378
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:10 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,31 +1,31 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'data_export'
-    MODULE_NAME = _('Data Import/Export')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'cloud-download-outline'
-    MODULE_DESCRIPTION = _('Data import and export in multiple formats')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'analytics'
+MODULE_ID = 'data_export'
+MODULE_NAME = _('Data Import/Export')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'cloud-download-outline'
+MODULE_DESCRIPTION = _('Data import and export in multiple formats')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'analytics'
 
-    MENU = {
-        'label': _('Data Import/Export'),
-        'icon': 'cloud-download-outline',
-        'order': 76,
-    }
+MENU = {
+    'label': _('Data Import/Export'),
+    'icon': 'cloud-download-outline',
+    'order': 76,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Import'), 'icon': 'cloud-upload-outline', 'id': 'import'},
 {'label': _('Export'), 'icon': 'cloud-download-outline', 'id': 'export'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'data_export.view_datajob',
+PERMISSIONS = [
+    'data_export.view_datajob',
 'data_export.run_import',
 'data_export.run_export',
 'data_export.manage_settings',
-    ]
+]
